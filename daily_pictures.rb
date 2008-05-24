b28ead1292745d97f0dedc6d074fde7d95e6cbba
Paul Davis <net.prophet.nz@gmail.com>
2008-05-24 15:18:40 +1200

    showing 7 picture to a page

--- daily_pictures.rb
+++ daily_pictures.rb
@@ -9,7 +9,7 @@ module DailyPictures::Controllers # handles url's
   # handle root url, eg. http://localhost:3301/
   class Index < R '/' 
     def get
-      @d = Date.today
+      @date = Date.today
       render :picture
     end
   end
@@ -17,8 +17,8 @@ module DailyPictures::Controllers # handles url's
   # handle specific date, eg. http://localhost:3301/picture/2006/10/20
   # also handle just year, or just year and month
   class Picture < R '/picture/(\d+)/(\d+)/(\d+)', '/picture/(\d+)/(\d+)', '/picture/(\d+)'
-    def get(year = "2008", month = "12", day = "01")
-      @d = Date.new(year.to_i, month.to_i, day.to_i)
+    def get(year = "2008", month = "01", day = "01")
+      @date = Date.new(year.to_i, month.to_i, day.to_i)
       render :picture
     end
   end
@@ -37,14 +37,25 @@ module DailyPictures::Views # handles views
   end
 
   def picture
-    require 'redcloth'
-    r = RedCloth.new "h1. Pictures\n\n"
-    r << "p. *The picture date* is: _#{@d.to_s}_"
+    require 'redcloth' # http://hobix.com/textile/ 
+    r = RedCloth.new "h1=. Pictures\n\n"
+    r << "p=. *The picture date* is: _#{@date.to_s}_\n\n"
+    #r << picture_links
+    6.downto(0) do |i|
+      d = @date - i
+      r << "p=. !#{d.strftime(DailyPictureSource)}!\n\n" 
+    end
+    #r << picture_links
     r.to_html
-  end
+  end 
   
+  def picture_links
+    # Prev Week  Prev Day  From x to y Next Week Next Day
+    ""
+  end
 end
 
 if __FILE__ == $0
    puts DailyPictures.run
 end
+
